a377b7acc1df48828427072c3fd4569f254ac8dc
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2022-01-04 23:29:02 +0200

    compile assets

--- public/phoneNumbers-index.js
+++ public/phoneNumbers-index.js
@@ -1,12 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!********************************************!*\
-  !*** ./resources/js/phoneNumbers-index.js ***!
-  \********************************************/
-document.querySelectorAll('#filter_form select').forEach(function (filterElement) {
-  filterElement.addEventListener('change', function () {
-    document.querySelector('#filter_form').submit();
-  });
-});
-/******/ })()
-;
+document.querySelectorAll("#filter_form select").forEach((function(e){e.addEventListener("change",(function(){document.querySelector("#filter_form").submit()}))}));
